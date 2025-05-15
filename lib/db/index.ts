bbd35d0dfdbe54bcb73fa881ad11e574c2150b8c
asimqasmi <kaabteacher@gmail.com>
2025-05-15 06:08:50 +0400

    fix env

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -4,8 +4,15 @@ import { createClient } from "@libsql/client";
 
 import { env } from "@/lib/env.mjs";
 
-export const sqlite = createClient({
-  url: env.DATABASE_URL!,
-});
+const config =
+  env.NODE_ENV === "development"
+    ? {
+      url: env.DATABASE_URL!,
+    }
+    : {
+      url: env.TURSO_DATABASE_URL,
+      authToken: env.TURSO_AUTH_TOKEN,
+    };
+export const sqlite = createClient(config);
 
 export const db = drizzle(sqlite);
